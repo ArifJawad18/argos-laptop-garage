a502c6fdc48b8bf0d1702fa28a37892c9e3ab83c
Arifjawad18 <arif.jawad18@gmail.com>
2022-12-05 03:26:44 +0600

    order card

--- src/Pages/Services/Service.js
+++ src/Pages/Services/Service.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 
 const Service = ({service}) => {
-	const {name, description, img,price} = service;
+	const {title, description, img,price} = service;
     return (
 	<div>
 		<div className="card w-96 bg-base-100 shadow-xl">
@@ -11,9 +11,9 @@ const Service = ({service}) => {
     <img src={img}alt="" className="rounded-xl" />
   </figure>
   <div className="card-body items-center text-center">
-    <h2 className="card-title font-bold">{name}</h2>
+    <h2 className="card-title font-bold">{title}</h2>
     <p>{description}</p>
-    <h2 className='font-bold text-blue-600'> Price: {price}</h2>
+    <h2 className='font-bold text-blue-600'> Price:$ {price}</h2>
     <div className="card-actions">
 	<button type="button" className="px-5 py-3 font-semibold rounded-full bg-blue-500 text-gray-100"> <a href='/'>ORDER NOW</a></button>
     </div>
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,59 +1,31 @@
-import React from 'react';
-import apple from '../Services/apple.png.jpg';
-import dell from '../Services/dell.png.png';
-import Hw from '../Services/Hw-png.jpg';
+import React, { useEffect, useState } from 'react';
 import Service from './Service';
 
 const Services = () => {
-    const ServicesData = [
-        {
-            id:1,
-            name: 'Apple',
-            description: 'New apple',
-            img: apple ,
-            price:'$100'
-        },
-        {
-            id:2,
-            name: 'Dell',
-            description: 'New Dell',
-            img: dell ,
-            price:'$200'
-        },
-        {
-            id:3,
-            name: 'Huawei',
-            description: 'New Huawei',
-            img: Hw ,
-            price:'$300'
-        },
-    ]
+    const [services, setServices] = useState([]);
+    useEffect( () =>{
+        fetch('services.json')
+        .then(res =>res.json())
+        .then(data => setServices(data))
+
+    },[])
 
     return (
-
         <div>
-             <div>
+          
+            <div>
                 <h2 className ='text-3xl text-center uppercase font-bold text-blue-500'> Our Product </h2>
              </div>
-
-             
-             <div className='grid gap-5 ml-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-                {
-                    ServicesData.map(service => <Service
-                    key={service.id}
-                    service={service}
-                    ></Service>)
-                }
-
+             <div className='grid gap-6 grid-cold-1 md:grid-cols-2 lg:grid-cols-3'>
+               {
+                services.map(service => <Service
+                key={service._id}
+                service={service}
+                ></Service>)
+               }
              </div>
-             <div className=' mt-4 mb-4 flex justify-center'>
-         <button type="button" className="px-8 py-3 font-semibold rounded-full bg-red-400 text-gray-100"> <a href='/product'>SEE ALL PRODUCT</a></button>
-         </div>
-
-             
-      
-        
-      </div>
+            
+        </div>
     );
 };
 
